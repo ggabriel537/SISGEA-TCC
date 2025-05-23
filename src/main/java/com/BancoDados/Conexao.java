b64625338d5b04409a002980a1101cd3d35f9760
Gabriel Guimaraes <dipirona28@gmail.com>
2025-05-23 11:58:04 -0400

    Conexao com o DB

---
+++ src/main/java/com/BancoDados/Conexao.java
@@ -0,0 +1,22 @@
+package com.BancoDados;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+
+    private static final String url = "jdbc:postgresql://localhost:5432/sisgea";
+    private static final String usuario = "postgres";
+    private static final String senha = "sisgea123";
+    //criar um arquivo para guardar os dados da conexao com o banco de dados
+    public static Connection getConexao() {
+        Connection conexao = null;
+        try {
+            conexao = DriverManager.getConnection(url, usuario, senha);
+        } catch (SQLException e) {
+            System.err.println("Erro ao conectar ao banco de dados: " + e.getMessage());
+        }
+        return conexao;
+    }
+}
