4ee11034738edb97b018a0e1dcffe18ecb04ae8d
Gabriel Guimaraes <dipirona28@gmail.com>
2025-05-23 10:14:14 -0400

    first commit

---
+++ src/main/java/com/sisgea/sisgea/SisgeaApplication.java
@@ -0,0 +1,13 @@
+package com.sisgea.sisgea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisgeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisgeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sisgea/sisgea/SisgeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sisgea.sisgea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisgeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
