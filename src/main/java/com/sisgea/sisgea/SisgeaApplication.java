09381267633f37683db8173c9fa4bad1a84cc160
Dipirona537 <dipirona28@gmail.com>
2025-10-16 22:23:04 -0400

    Adiciona lógica para criação de um administrador inicial se não existir nenhum administador cadastrado

--- src/main/java/com/sisgea/sisgea/SisgeaApplication.java
+++ src/main/java/com/sisgea/sisgea/SisgeaApplication.java
@@ -1,14 +1,36 @@
 package com.sisgea.sisgea;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.sisgea.BancoDados.Controllers.AdministradorController;
+import com.sisgea.Entidades.Administrador;
+import com.sisgea.Entidades.Usuario;
 
 @SpringBootApplication
 @EnableScheduling
 public class SisgeaApplication {
 
 	public static void main(String[] args) {
+		gerarAdminInicial();
 		SpringApplication.run(SisgeaApplication.class, args);
+	}
+
+	private static void gerarAdminInicial(){
+		List<Administrador> admins = AdministradorController.listarAdministradores();
+		if(!admins.isEmpty()) return;
+		Administrador admin = new Administrador();
+		Usuario usuario = new Usuario();
+		usuario.setPermissao(1);
+		usuario.setUsuario("sisgea");
+		usuario.setSenha("sisgea123");
+		admin.setNome("Administrador Geral");
+		admin.setAtivo(true);
+		admin.setUsuario(usuario);
+		AdministradorController.salvarAdministrador(admin);
+		System.out.println("Nenhum administrador encontrado. Administrador inicial criado!");
 	}
 }
